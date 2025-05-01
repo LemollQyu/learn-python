b1cd58be4d0748d0b349860f0695271f6ebe4b97
Annas <lemoll.qyu@gmail.com>
2025-05-01 23:09:58 +0700

    tambah data tuples dan sets

---
+++ learn-satu/tuple-dan-set/main.py
@@ -0,0 +1,31 @@
+print("tuple dan set")
+
+# tuples datanya
+data = (7,4,3,10)
+
+print(data)
+
+print(data[1])
+
+
+# tapi data tuple itu tidak bisa dirubah datanya
+# data[1] = 10 
+
+# data tuple juga tidak bisa di tambah dengan append
+# data.append(7)
+
+# code itu akan error
+
+# fungsi ini adalah berguna ketika data itu constanta
+
+
+
+# data sets
+data_sets = {1,2,3,4,5,6,7}
+print(data_sets)
+
+# kalo data sets itu data per index ga bisa diambil
+# print(data_sets[2])
+# code itu akan error
+
+
